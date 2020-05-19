352a4ed965a469af3a4ef073b7c0ea929d9b3531
marian_moise <marian_moise@ymail.com>
2020-05-19 13:27:43 +0300

    insertion sort

--- sorting.java
+++ sorting.java
@@ -3,7 +3,7 @@ buble sort:
 int[] array = {8, 7, 5, 11, 15, 3};
 
 for (int i = 0; i < array.length; i++) {
-    for (int j = i + 1; i < array.length; j++) {
+    for (int j = i + 1; j < array.length; j++) {
     	if (array[i] > array[j]) {
     		int temp = array[i];
     		array[i] = array[j];
@@ -13,3 +13,20 @@ for (int i = 0; i < array.length; i++) {
 }
 
 System.out.println(Arrays.toString(array));
+
+
+//#1 - Sort 10 schools around your house by distance:
+
+//#2 - eBay sorts listings by the current Bid amount:
+
+//#3 - Sport scores on ESPN
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+
+//#6 - Temperature Records for the past 50 years in Canada
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+
+//#8 - You want to teach sorting for the first time
--- src/main/java/Section13_Algorithms_Sorting/InsertionSort/InsertionSort.java
+++ src/main/java/Section13_Algorithms_Sorting/InsertionSort/InsertionSort.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] array = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+//        int[] array = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+        int[] array = {99, 44, 6, 2, 1, 5};
         insertSort(array);
 
     }
@@ -14,12 +15,13 @@ public class InsertionSort {
         for (int i = 1; i < array.length; i++) {
             int key = array[i];
             int j = i - 1;
-            while (j >= 0 && array[j] > key) {
-                array[j + 1] = array[j];
-                j = j - 1;
+            while (j >= 0 && key < array[j]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                j--;
                 counter++;
             }
-            array[j + 1] = key;
         }
         System.out.println(Arrays.toString(array));
         System.out.println(counter);
