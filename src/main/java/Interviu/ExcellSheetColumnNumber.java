cbc1b42a6ff652936f641f4ff68d6f7c7edbd848
marian_moise <marian_moise@ymail.com>
2020-06-13 15:55:23 +0300

    updateLeetcode

--- src/main/java/Interviu/ExcellSheetColumnNumber.java
+++ src/main/java/Interviu/ExcellSheetColumnNumber.java
@@ -1,25 +1,25 @@
-package Interviu;
-
-public class ExcellSheetColumnNumber {
-    public static void main(String[] args) {
-        String s = "ZY";
-        System.out.println(titleToNumber(s));
-
-        String s2 = "A";
-        char[] chars2 = s2.toCharArray();
-        int number = 0;
-        //number = number + chars2[0] - 'A';
-        System.out.println(
-                (chars2[0] + 1) - 'A');
-    }
-
-    public static int titleToNumber(String s) {
-
-        char[] chars = s.toCharArray();
-        int number = 0;
-        for (int i = 0; i < chars.length ; i++) {
-            number +=  (((chars[i] + 1) - 'A') * Math.pow(26, chars.length - 1 -i));
-        }
-        return number;
-    }
-}
+//package Interviu;
+//
+//public class ExcellSheetColumnNumber {
+//    public static void main(String[] args) {
+//        String s = "ZY";
+//        System.out.println(titleToNumber(s));
+//
+//        String s2 = "A";
+//        char[] chars2 = s2.toCharArray();
+//        int number = 0;
+//        //number = number + chars2[0] - 'A';
+//        System.out.println(
+//                (chars2[0] + 1) - 'A');
+//    }
+//
+//    public static int titleToNumber(String s) {
+//
+//        char[] chars = s.toCharArray();
+//        int number = 0;
+//        for (int i = 0; i < chars.length ; i++) {
+//            number +=  (((chars[i] + 1) - 'A') * Math.pow(26, chars.length - 1 -i));
+//        }
+//        return number;
+//    }
+//}
---
+++ src/main/java/Interviu/FirstUniqueChar.java
@@ -0,0 +1,28 @@
+package Interviu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUniqueChar {
+    public static void main(String[] args) {
+
+        String s = "leetcode";
+        System.out.println(firstUniqChar(s));
+
+
+    }
+
+    public static int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
