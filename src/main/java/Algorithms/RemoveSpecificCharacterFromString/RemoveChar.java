98e9b03601d61cb149e56249ad5b9b87937b628d
marian_moise <marian_moise@ymail.com>
2022-02-13 10:27:32 +0200

    remove char from string

--- src/main/java/Algorithms/RemoveSpecificCharacterFromString/RemoveChar.java
+++ src/main/java/Algorithms/RemoveSpecificCharacterFromString/RemoveChar.java
@@ -3,8 +3,11 @@ package Algorithms.RemoveSpecificCharacterFromString;
 public class RemoveChar {
     public static void main(String[] args) {
         String s = "Ego Zenovius votum posui";
+        //using custom method
         System.out.println(removeCharacter(s, 'v'));
         System.out.println(removeCharacter(s, ' '));
+        //using replaceAll
+        System.out.println(s.replaceAll("E", ""));
     }
 
     public static String removeCharacter(String s, char c) {
